0557db621569bae00752d7b46500b5dbc9be289b
Rasel Hossen <raselhossen052@gmail.com>
2024-02-05 14:27:44 +0600

    features/implement_system_color_scheme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,9 +1,29 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+
+  const [darkMode, setDarkMode] = useState(prefersDarkMode);
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    );
+
+    const handleDarkModeChange = (e) => {
+      setDarkMode(e.matches);
+    };
+
+    darkModeMediaQuery.addEventListener('change', handleDarkModeChange);
+
+    return () => {
+      darkModeMediaQuery.removeEventListener('change', handleDarkModeChange);
+    };
+  }, []);
 
   const toggleDarkMode = () => {
     setDarkMode((prevDarkMode) => !prevDarkMode);
@@ -11,7 +31,7 @@ export const DarkModeProvider = ({ children }) => {
 
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
-      <div className={darkMode ? 'dark  !bg-[#191D26]' : 'bg-slate-200'}>
+      <div className={darkMode ? 'dark bg-[#191D26]' : 'bg-slate-200'}>
         {children}
       </div>
     </DarkModeContext.Provider>
